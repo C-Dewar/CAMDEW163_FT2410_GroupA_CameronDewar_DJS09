93cf655c4f72ae5ea59e18fc5c18249cb62d9d3f
C-Dewar <camdewcode@gmail.com>
2025-04-10 12:56:52 +0200

    Adding the properties to the dashboard

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,113 @@
 // Write a function that will only accept numbers and attend to
 // all TypeScript weakness flags.
 // : number
-const reviewTotalDisplay = document.querySelector("#reviews");
+
+import { showReviewTotal, populateUser } from './utils';
+let isOpen: boolean;
+
+const reviewTotalDisplay = document.querySelector('#reviews');
 
 const reviews = [
   {
-    name: "Sheia",
+    name: 'Sheia',
     stars: 5,
     loyaltyUser: true,
-    date: "01-04-2021",
+    date: '01-04-2021',
   },
   {
-    name: "Andrzej",
+    name: 'Andrzej',
     stars: 3,
     loyaltyUser: false,
-    date: "28-03-2021",
+    date: '28-03-2021',
   },
   {
-    name: "Omar",
+    name: 'Omar',
     stars: 4,
     loyaltyUser: true,
-    date: "27-03-2021",
+    date: '27-03-2021',
   },
 ];
+
+const you: {
+  firstName: string;
+  lastName: string;
+  isReturning: boolean;
+  age: number;
+  stayedAt: string[];
+} = {
+  firstName: 'Cam',
+  lastName: 'Dewar',
+  isReturning: true,
+  age: 33,
+  stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow'],
+};
+
+const properties: {
+  image: string;
+  title: string;
+  price: number;
+  location: {
+    firstLine: string;
+    city: string;
+    code: number;
+    country: string;
+  };
+  contact: string;
+  isAvailable: boolean;
+}[] = [
+  {
+    image: '../images/colombia-property.jpg',
+    title: 'Colombian Shack',
+    price: 45,
+    location: {
+      firstLine: 'shack 37',
+      city: 'Bogota',
+      code: 45632,
+      country: 'Colombia',
+    },
+    contact: 'marywinkle@gmail.com',
+    isAvailable: true,
+  },
+  {
+    image: '../images/poland-property.jpg',
+    title: 'Polish Cottage',
+    price: 34,
+    location: {
+      firstLine: 'no 23',
+      city: 'Gdansk',
+      code: 343903,
+      country: 'Poland',
+    },
+    contact: 'garydavis@hotmail.com',
+    isAvailable: false,
+  },
+  {
+    image: 'images/london-property.jpg',
+    title: 'London Flat',
+    price: 23,
+    location: {
+      firstLine: 'flat 15',
+      city: 'London',
+      code: 35433,
+      country: 'United Kingdom',
+    },
+    contact: 'andyluger@aol.com',
+    isAvailable: true,
+  },
+];
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
+populateUser(you.isReturning, you.firstName);
+
+//Adding the properties
+
+for (let i = 0; i < properties.length; i++) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.innerHTML = properties[i].title;
+  const image = document.createElement('img');
+  image.setAttribute('src', properties[i].image);
+  card.appendChild(image);
+  propertyContainer.appendChild(card);
+}
